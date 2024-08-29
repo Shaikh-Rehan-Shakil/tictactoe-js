87bf0550a80b807ceee180e626a69ccc7b84e030
rehanshaikh25 <shakilrehan23@gmail.com>
2024-08-29 20:17:44 +0530

    unfinished commit need to get green square

---
+++ main.js
@@ -0,0 +1,17 @@
+const cells = document.querySelector(".cells");
+const status = cells.querySelector(".status");
+const resetBtn = cells.querySelector(".reset");
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+let options = ["","","","","","","","",""];
+let currentPlayer = "X";
+let gameOver = false;
